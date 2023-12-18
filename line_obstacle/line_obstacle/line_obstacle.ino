920aed7b173637a45340db2c0a7dd57934f27850
pl1006321 <pl1006321@ahschool.com>
2023-12-18 10:19:47 -0500

    final upload

---
+++ line_obstacle/line_obstacle/line_obstacle.ino
@@ -0,0 +1,184 @@
+#include <Arduino.h>
+#include <MeMCore.h> 
+
+MeUltrasonicSensor ultrasonic(PORT_3);
+MeLineFollower linefinder(PORT_2);
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2); 
+
+int value, i; 
+float howfar; 
+
+void movebackward(int d)
+{
+  motor1.run(100);
+  motor2.run(-100);
+  delay(d); 
+  motor1.stop();
+  motor2.stop();
+}
+
+void moveforward(int d) 
+{ 
+  motor1.run(-100);
+  motor2.run(100); 
+  delay(d); 
+  motor1.stop();
+  motor2.stop();  
+}
+
+void spincw(int d) 
+{
+  motor1.run(-100);
+  motor2.run(-100); 
+  delay(d); 
+  motor1.stop();
+  motor2.stop();
+}
+
+void spinccw(int d) 
+{
+  motor1.run(100);
+  motor2.run(115); 
+  delay(d); 
+  motor1.stop();
+  motor2.stop();
+}
+
+void left(int d)
+{
+  motor1.run(100);
+  motor2.run(100);  
+  delay(100); 
+  motor1.stop();
+  motor2.stop(); 
+  moveforward(100); 
+}
+
+void right(int d) // d = 1250 was optimal for this 
+{
+  motor1.run(-100); 
+  motor2.run(-100);
+  delay(100);
+  motor1.stop(); 
+  motor2.stop(); 
+  moveforward(100); 
+}
+
+void linesensor() 
+// black = blue light off = 0
+// white = blue light on = 1
+{
+if (linefinder.readSensor1()==0 && linefinder.readSensor2()==0)
+  {
+    // Serial.println("Sensor 1 on black and Sensor 2 on black"); 
+    value = 1; 
+  }
+
+  else if (linefinder.readSensor1()==1 && linefinder.readSensor2()==0)
+  {
+    // Serial.println("Sensor 1 on white and Sensor 2 on black"); 
+    value = 2;
+  }
+
+  else if (linefinder.readSensor1()==0 && linefinder.readSensor2()==1)
+  {
+    // Serial.println("Sensor 1 on black and Sensor 2 on white"); 
+    value = 3; 
+  }
+
+  else // (linefinder.readSensor1()==1 && linefinder.readSensor2()==1)
+  {
+    // Serial.println("Sensor 1 on white and Sensor 2 on white"); 
+    value = 4;
+  }
+}
+
+void checkforstop()
+{
+moveforward(300); 
+  for (i=0;i<20;i++) {
+    spincw(35);
+    linesensor();
+    if (value==1) {
+     return;
+     } 
+}
+  for (i=0;i<40;i++) {
+    spinccw(35); 
+    linesensor();
+    if (value==1) {
+      return; }
+}
+}
+
+void obstaclechecker()
+{
+  howfar = ultrasonic.distanceCm(); 
+  Serial.println(howfar);
+  return;
+}
+
+void avoiding()
+{
+  if (howfar <= 7)
+  {
+    motor1.stop();
+    motor2.stop();
+    spincw(600);
+    moveforward(1000);
+    spinccw(600);
+    moveforward(500);
+    spinccw(600); 
+    moveforward(1000); 
+    checkforstop();
+    return;
+  }
+ return;
+}
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+while (value!=5)
+{
+linesensor();
+
+ movement:
+
+ switch (value) 
+ {
+   case 1:
+     moveforward(100);
+     obstaclechecker();
+     avoiding();
+     break;
+ 
+   case 2: 
+     right(100); 
+     obstaclechecker();
+     avoiding();
+     break;
+ 
+   case 3:
+     left(100); 
+     obstaclechecker();
+     avoiding();
+     break;
+ 
+   case 4:
+     checkforstop(); 
+     if (value==5)
+     {
+       motor1.stop();
+       motor2.stop();
+     }
+     
+     break;
+}
+}
+
+
+}

3cc10f7fb4f67a4645d9ebd8525c4e64bb761a59
pl1006321 <pl1006321@ahschool.com>
2024-01-30 10:45:54 -0500

    edit

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+firstcolor = str(input('Enter a color: '))
+secondcolor = str(input('Enter another color: '))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+firstcolor = firstcolor.lower()
+firstcolor = firstcolor.strip()
+secondcolor = secondcolor.lower()
+secondcolor = secondcolor.strip()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+colors = ['red', 'blue', 'yellow']
+if firstcolor != colors:
+    print("did this work")
+else:
+    print("erm i dont think it works")

99a19c63175f0aaab869d09aecec67449e8f8409
pl1006321 <pl1006321@ahschool.com>
2024-02-08 10:38:55 -0500

    commit

---
+++ rock paper scissors/main.py
@@ -0,0 +1,5 @@
+# random number is generated from the range of 1-3.
+# 1 is rock, 2 is papers, and 3 is scissors
+# do NOT display the computers choice yet
+# the user enters "rock" "paper" or "scissors"
+# if both players make the same choice, the game must be played again to determine the winner

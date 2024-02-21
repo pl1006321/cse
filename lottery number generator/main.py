92a15aac4ced3103d443eebfdd875b7544a54a85
mandy wu <149692472+pl1006321@users.noreply.github.com>
2024-02-21 10:29:27 -0500

    second update

--- lottery number generator/main.py
+++ lottery number generator/main.py
@@ -1,48 +1,51 @@
 import random
 
-
-# the program should generate 7 random numbers, each in the range of 0-9
+# the program should generate 7 random numbers, each in the range of 0-9 
 # then it should assign each number to a list element
 # then write another loop that displays the contents of the list
 # have the user input a 7 digit number into a list and output which numbers 
 # match the program's numbers. if all numbers match, the user should be told they 
 # won the lottery! 
 
-
 def lottery_generator():
-    nums = []
-    for i in range(7):
-        num = random.randint(0, 9)
-        nums.append(num)
-    return nums
-
-
-# numbers have to be distinct. killing myself
+  nums = []
+  while len(nums) < 7:
+    num = random.randint(0,9)
+    if num not in nums:
+      nums.append(num)
+  return nums
+  
 lotterynums = lottery_generator()
-print(lotterynums)
-
+print(lotterynums) 
 
 def userinput():
-    entry = str(input('Enter 7 numbers, each separated by a space: '))
-    entry = entry.strip()
-    usernums = entry.split(' ')
-    return usernums
+  entry = str(input('Enter 7 distinct numbers, each separated by a space: '))
+  entry = entry.strip()
+  usernums = entry.split(' ')
+  return usernums
 
+userentry = userinput() # do i need to do input validation...????
+isusersped = 0
 
-userentry = userinput()
-# user input also has to b distinct hello what/?!?!??!?!?!?
+for x in userentry:
+  if userentry.count(x) != 1:
+    isusersped = 1
+
+while isusersped == 1:
+  print("The numbers entered were not distinct. Please try again.")
+  userentry = userinput()
+  for x in userentry:
+    if userentry.count(x) != 1:
+      isusersped = 1
+    else: 
+      isusersped = 0
 
 correct = 0
 
-for i in (userentry):
-    for j in (lotterynums):
-        if int(i) == j:
-            correct += 1
-            break
-        else:
-            continue
+for x in userentry:
+  if int(x) in lotterynums:
+    correct += 1
 
 if correct == 7:
-    print('You have won the lottery!')
-else:
-    print('You guessed', correct, 'numbers correctly!')
+  print('You have won the lottery!')  
+else: print('You guessed', correct, 'numbers correctly!') 

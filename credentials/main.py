ef26373d1cce46ae30acb595097a52baa22a5a65
pl1006321 <pl1006321@ahschool.com>
2024-02-23 10:45:01 -0500

    IM DONE

--- credentials/main.py
+++ credentials/main.py
@@ -7,6 +7,8 @@
 # must contain 3 functions: create username, create password, and save password
 # maybe create function for verifying password and email??
 
+import os
+
 def verify_username(entry):
     if "@" in entry and "." in entry:
         return 1
@@ -16,12 +18,38 @@ def verify_username(entry):
 def create_username():
     email = str(input("Please enter a valid email address: "))
     validity = verify_username(email.strip())
-    if validity != 1:
-        print("Username is not valid. Please try again. ")
+    if not validity:
+        while validity == 0:
+          print("Username is not valid. Please try again. ")
+          email = str(input("Please enter a valid email address: "))
+          validity = verify_username(email.strip())
     else: return email
 
+def verify_password(entry):
+  if entry.isupper() or entry.islower():
+    return False
+  if len(entry) < 8 or entry.isalpha() or entry.isdecimal():
+    return False
+  else: return True
 
-isusersped = 1
-while isusersped == 1:
-    username = create_username()
-    if type(username) != str
+
+def create_password():
+  print("Password must contain: \n -at least one uppercase letter \n -at least one lowercase letter \n -at least one numeric digit \n -at least 8 total characters")
+  password = str(input("Please enter a valid password: "))
+  validity = verify_password(password)
+  if not validity:
+    while validity == 0:
+      print("Password is not valid. Please try again. ")
+      password = str(input("Please enter a valid password: "))
+      validity = verify_password(password)
+  else: return password
+
+validusername = create_username()
+print("Username has been saved.")
+validpassword = create_password()
+print("Password has been saved.")
+
+f=open("passwords.txt", "a")
+f.write("\n Username: " + str(validusername))
+f.write("\n Password: " + str(validpassword))
+f.close()
